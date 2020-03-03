a742a3058f04a6d106d1c4c03c459a6e458c6cee
Hiragi-GKUTH <45023631+HiragiGKUTH@users.noreply.github.com>
2020-03-03 17:53:06 +0900

    add cors support (#34)

--- server/src/api/main.py
+++ server/src/api/main.py
@@ -1,8 +1,11 @@
 import uvicorn
 from fastapi import FastAPI
 from routers import login, index, test
+from middlewares import cors
 
 app = FastAPI()
+
+app = cors.setup(app)
 
 app.include_router(index.router)
 app.include_router(login.router)
