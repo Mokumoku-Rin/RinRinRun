19d5f17b95bd723b093b071afe77fad6f4261ddd
Hiragi-GKUTH <hiragi.gkuth.dev@gmail.com>
2020-03-03 16:29:55 +0900

    add test routing

--- server/src/api/main.py
+++ server/src/api/main.py
@@ -1,11 +1,14 @@
 import uvicorn
 from fastapi import FastAPI
-from routers import login, index
+from routers import login, index, test
 
 app = FastAPI()
 
 app.include_router(index.router)
 app.include_router(login.router)
+
+# for testing
+app.include_router(test.router, prefix="/test")
 
 if __name__ == "__main__":
   uvicorn.run(app, host="0.0.0.0", port=8081)
