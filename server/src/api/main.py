b24ee7ee65ba92845aeab2b5cc4d17341813849e
KIBE <30930566+kibehiro@users.noreply.github.com>
2020-03-03 21:47:03 +0900

    uidが取得できるように (#36)
    * keyを追加
    
    * firebase-adminをビルドできるように
    
    * とりあえずuidが表示できる
    
    * firebaseの認証を一度だけに変更

--- server/src/api/main.py
+++ server/src/api/main.py
@@ -2,6 +2,7 @@ import uvicorn
 from fastapi import FastAPI
 from routers import login, index, test
 from middlewares import cors
+import firebase_admin
 
 app = FastAPI()
 
@@ -14,4 +15,5 @@ app.include_router(login.router)
 app.include_router(test.router, prefix="/test")
 
 if __name__ == "__main__":
+  default_app = firebase_admin.initialize_app()
   uvicorn.run(app, host="0.0.0.0", port=8081)
--- server/src/api/services/test.py
+++ server/src/api/services/test.py
@@ -1,7 +1,11 @@
 from schemas.test import TestSchema
+from firebase_admin import auth
 
 class TestService:
   @staticmethod
   def create_test_response(req: TestSchema):
-    print(req)
+    print('Req:', req)
+    decoded_token = auth.verify_id_token(req.token)
+    uid = decoded_token['uid']
+    print('uid:', uid)
     return {"result": "received"}
