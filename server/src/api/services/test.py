3ea3cb9e80bfb1399e058fc4dde2098a572b3131
KIBE <30930566+kibehiro@users.noreply.github.com>
2020-03-03 21:16:04 +0900

    とりあえずuidが表示できる

--- server/src/api/services/test.py
+++ server/src/api/services/test.py
@@ -1,7 +1,14 @@
 from schemas.test import TestSchema
+import firebase_admin
+from firebase_admin import auth
+from firebase_admin import credentials
 
 class TestService:
   @staticmethod
   def create_test_response(req: TestSchema):
-    print(req)
+    print('Req:', req)
+    default_app = firebase_admin.initialize_app()
+    decoded_token = auth.verify_id_token(req.token)
+    uid = decoded_token['uid']
+    print('uid:', uid)
     return {"result": "received"}
