25db12428979fd9dadf9c01aa7372bb65fe9c1d9
Hiragi-GKUTH <45023631+HiragiGKUTH@users.noreply.github.com>
2020-03-04 00:40:23 +0900

    Add auth test (#40)
    * add auth deps
    
    * delete index route
    
    * update for login sessions
    
    * fix syntax miss

--- server/src/api/routers/login.py
+++ server/src/api/routers/login.py
@@ -2,6 +2,6 @@ from fastapi import APIRouter
 
 router = APIRouter()
 
-@router.get("/login/")
+@router.get("/")
 async def get_login():
-  return {"page": "login"}
+  return "get login"
--- server/src/api/routers/session.py
+++ server/src/api/routers/session.py
@@ -3,5 +3,5 @@ from fastapi import APIRouter
 router = APIRouter()
 
 @router.get("/")
-async def get_index():
-  return {"page": "index"}
+async def get_session():
+  return "logged in"
