f34254b0ae3ce17d542eb198b513704036fe98b2
Hiragi-GKUTH <hiragi.gkuth.dev@gmail.com>
2020-03-04 15:10:14 +0900

    update to use usecase

--- server/src/api/depends/auth.py
+++ server/src/api/depends/auth.py
@@ -1,14 +1,12 @@
 from fastapi import Header, HTTPException, Response
 from firebase_admin import auth, exceptions
 
+from usecases.auth import verify_token
+
 async def verify_firebase_token(x_token: str = Header(...)):
   if x_token == None:
     raise HTTPException(403, detail="X-Token verification error: not logged in")
-  
-  try:
-    dec_token = auth.verify_id_token(x_token)
-  except exceptions.FirebaseError as e:
-    raise HTTPException(e.code, "X-Token verification error: " + e.message)
+  uid = verify_token(x_token)
 
   # and then authentication is OK
   Response.headers.append("X-Token", x_token)
