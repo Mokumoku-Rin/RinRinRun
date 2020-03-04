79ec75de433c0f288874d72c9180688357673204
Hiragi-GKUTH <hiragi.gkuth.dev@gmail.com>
2020-03-04 16:35:52 +0900

    add sample cource1 geojson data

--- db/migration/12-create_cources.sql
+++ db/migration/12-create_cources.sql
@@ -1,6 +1,7 @@
 CREATE TABLE IF NOT EXISTS `mokumoku`.`cources` (
 	`id` INT unsigned NOT NULL AUTO_INCREMENT,
-	`landmark_id` INT unsigned NOT NULL,
+	`name` VARCHAR(64) NOT NULL,
+	`landmark_geojson` TEXT NOT NULL,
 	`played_count` INT unsigned NOT NULL DEFAULT '0',
 	`difficulity` TINYINT unsigned NOT NULL DEFAULT '0',
 	`created_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
