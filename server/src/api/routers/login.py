d44104f88784478c9ca199a44750a21e5ffd9fd8
Hiragi-GKUTH <hiragi.gkuth.dev@gmail.com>
2020-03-04 15:11:03 +0900

    add login routing

--- server/src/api/routers/login.py
+++ server/src/api/routers/login.py
@@ -1,7 +1,20 @@
 from fastapi import APIRouter
+from schemas.login import LoginRequest, LoginResponse
+from services.login import LoginService
 
 router = APIRouter()
 
 @router.get("/")
 async def get_login():
   return "get login"
+
+@router.post("/", response_model=LoginResponse)
+async def post_login(loginRequest: LoginRequest):
+  token = loginRequest.token
+  message = await LoginService.register(token)
+  print(message)
+  response: LoginRequest = {
+    "result": message
+  }
+
+  return response
