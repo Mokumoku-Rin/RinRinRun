a1586dccfc5aa16c9d76c3cb452e88b30d26d7fc
KIBE <30930566+kibehiro@users.noreply.github.com>
2020-03-05 20:01:55 +0900

    land_mark_visitにデータを挿入できるように ただしlandmark_idがダミーデータ

--- server/src/api/usecases/workout.py
+++ server/src/api/usecases/workout.py
@@ -1,5 +1,15 @@
-from db.workout import insert_workout_history
+from db.workout import insert_workout_history, get_workout_history_id, insert_landmark_visit
 
 
 def add_workout_history(uid, cource_id, geometry_track, time):
     insert_workout_history(uid, cource_id, geometry_track, time)
+
+
+def add_landmark_visit(uid, cource_id, geometry_track, time, route):
+    work_history_id = get_workout_history_id(
+        uid, cource_id, geometry_track, time)
+    print("work_history:", work_history_id)
+    for i in route:
+        landmark_id = 1  # 実際にはlandmarkテーブルからidを取ってくる必要がある
+        time = i.time
+        insert_landmark_visit(work_history_id['id'], landmark_id, time)
