e31849faf9c943a5bd5121dc2c397e201e46480c
Hiragi-GKUTH <45023631+HiragiGKUTH@users.noreply.github.com>
2020-03-05 18:02:41 +0900

    skip verification on development (#52)

--- server/src/api/depends/auth.py
+++ server/src/api/depends/auth.py
@@ -1,3 +1,5 @@
+import os
+from random import randint
 from fastapi import Header, HTTPException, Response
 from firebase_admin import auth, exceptions
 
@@ -7,4 +9,11 @@ class FirebaseToken:
   def __init__(self, x_token: str = Header(...)):
     if x_token == None:
       raise HTTPException(403, detail="X-Token verification error: not logged in")
+
+    # on development, skip token verification
+    if os.environ["SERVER_ENV"] == "development":
+      self.uid = "development_" + str(randint(0,1000000))
+      return
+
+    
     self.uid = verify_token(x_token)
