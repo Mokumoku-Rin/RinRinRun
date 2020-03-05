9f566d58abdb995989a52e4791d6cf4916db6df7
KIBE <30930566+kibehiro@users.noreply.github.com>
2020-03-05 15:45:41 +0900

    geometryの格納方法をテキストに

--- db/migration/11-create_workout_histories.sql
+++ db/migration/11-create_workout_histories.sql
@@ -1,10 +1,10 @@
 CREATE TABLE IF NOT EXISTS `mokumoku`.`workout_histories` (
-	`id` INT unsigned NOT NULL AUTO_INCREMENT,
-	`user_id` VARCHAR(64) NOT NULL,
-	`cource_id` INT unsigned NOT NULL,
-	`geometry_track` LINESTRING NOT NULL,
-	`time` TIME NOT NULL,
-	`created_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-	`updated_at` TIMESTAMP NOT NULL ON UPDATE CURRENT_TIMESTAMP,
-    PRIMARY KEY(`id`)
-) ENGINE=InnoDB;
+  `id` INT unsigned NOT NULL AUTO_INCREMENT,
+  `user_id` VARCHAR(64) NOT NULL,
+  `cource_id` INT unsigned NOT NULL,
+  `geometry_track` TEXT NOT NULL,
+  `time` TIME NOT NULL,
+  `created_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` TIMESTAMP NOT NULL ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY(`id`)
+) ENGINE = InnoDB;
