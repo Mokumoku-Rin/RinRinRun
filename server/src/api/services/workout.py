30c78423c31e1822b6ac3728014f4b81753ae768
KIBE <30930566+kibehiro@users.noreply.github.com>
2020-03-07 01:29:56 +0900

    parse部分を実装

--- server/src/api/services/workout.py
+++ server/src/api/services/workout.py
@@ -5,10 +5,13 @@ from usecases.workout import add_workout_history, add_landmark_visit, update_use
 class WorkoutService:
     @staticmethod
     async def workout_test(workout_request, uid):
-        cource_id = workout_request.course_id
-        geometry_track = workout_request.move_history
-        time = workout_request.total_time
-        route = workout_request.route
+        properties = workout_request.properties
+        time_list = properties.time_list
+        total_distance = properties.total_distance
+        total_time = properties.total_time
+        landmark_visits = workout_request.landmark_visits
+        geo_json = workout_request.geo_json
+
         add_workout_history(uid, cource_id, geometry_track, time)
         add_landmark_visit(uid, cource_id, geometry_track, time, route)
         return geometry_track
