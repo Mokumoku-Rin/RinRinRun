af37800064159899fcdf59f157b7c039d383f6ac
KIBE <30930566+kibehiro@users.noreply.github.com>
2020-03-06 14:58:43 +0900

    最低限Routingするように

--- server/src/api/routers/session.py
+++ server/src/api/routers/session.py
@@ -1,14 +1,20 @@
 from fastapi import APIRouter, Depends
 from depends.auth import FirebaseToken
-from routers.sessions import workout
+from routers.sessions import workout, image
 
 router = APIRouter()
 
 router.include_router(
-  workout.router,
-  prefix="/workout",
+    workout.router,
+    prefix="/workout",
 )
+
+router.include_router(
+    image.router,
+    prefix="/image",
+)
+
 
 @router.get("/")
 async def get_session(fbToken: FirebaseToken = Depends()):
-  return "logged in user"
+    return "logged in user"
---
+++ server/src/api/services/image.py
@@ -0,0 +1,4 @@
+class ImageService:
+    @staticmethod
+    async def ImageCompare(image):
+        return "Image"
