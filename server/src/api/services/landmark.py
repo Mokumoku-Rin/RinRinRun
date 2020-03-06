1a05a82f33293c12a638aeea5a58db08d369604e
KIBE <30930566+kibehiro@users.noreply.github.com>
2020-03-06 22:54:21 +0900

    変数名の見直し

--- server/src/api/services/landmark.py
+++ server/src/api/services/landmark.py
@@ -1,10 +1,10 @@
-from usecases.image import compare_image
+from usecases.image import calc_image_similarity
 
 
 class LandmarkService:
     @staticmethod
     async def ImageCompare(image_request):
-        similarity = compare_image(
+        similarity = calc_image_similarity(
             image_request.img, image_request.landmark_id)
         threshold = 0.5
         # similarityは、一致度が高いほど0に近くなるため
