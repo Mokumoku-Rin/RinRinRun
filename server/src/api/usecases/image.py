c45c2004112dffdd2acc68d655cccf6d52ecf1aa
KIBE <30930566+kibehiro@users.noreply.github.com>
2020-03-06 23:49:16 +0900

    英文法に習って修正

--- server/src/api/usecases/image.py
+++ server/src/api/usecases/image.py
@@ -5,25 +5,25 @@ import base64
 from db.landmarks import get_image_url
 
 
-def binary_convert_to_ndarray(binary):
+def convert_binary_to_ndarray(binary):
     npimg = np.fromstring(binary, dtype=np.uint8)
     img = cv2.imdecode(npimg, 1)
     return img
 
 
-def base64_decode_to_ndarray(image):
+def decode_base64_to_ndarray(image):
     img_binary = base64.b64decode(image)
-    img = binary_convert_to_ndarray(img_binary)
+    img = convert_binary_to_ndarray(img_binary)
     return img
 
 
 def calc_image_similarity(user_base64_image, landmark_id):
 
-    user_image = base64_decode_to_ndarray(user_base64_image)
+    user_image = decode_base64_to_ndarray(user_base64_image)
 
     database_image_url = get_image_url(landmark_id)['img_url']
     database_image_binary = requests.get(database_image_url).content
-    database_image = binary_convert_to_ndarray(database_image_binary)
+    database_image = convert_binary_to_ndarray(database_image_binary)
 
     # A-KAZEとかいう検出器を使う
     akaze = cv2.AKAZE_create()
