160bb0e53a6492b810cafa1a86069c6a436ff794
yuu <dptypo@gmail.com>
2020-03-06 23:03:32 +0900

    add dammiy data and time json post method

--- client/src/src/components/RinRinMap.vue
+++ client/src/src/components/RinRinMap.vue
@@ -5,8 +5,10 @@
     <button @click="startGetGPS">計測スタート</button>
     <button @click="stopGetGPS">計測ストップ</button>
     <button @click="clearGPSHistory">クリア</button>
-    <button @click="showGPSHistory">計測結果</button>
+    <button @click="showGPSHistory">GPS計測結果</button>
     <button @click="sendGPSHistory">計測結果を送信</button>
+    <br>
+    <button @click="test">test func</button>
 
   </div>
 </template>
@@ -24,12 +26,55 @@ L.Icon.Default.mergeOptions({
 
 const SOJO_GPS_POSITION = [34.878031, 135.575573]
 
+function calDistance(positionList){
+  let distance = 0.0
+
+  if(positionList.length < 2)return 0
+
+  for(let i=1; i<positionList.length; i++){
+    // 緯度経度をラジアンに変換
+    const radLatitudeA = deg2rad(positionList[i-1][0])
+    const radLongitudeA = deg2rad(positionList[i-1][1])
+    const radLatitudeB = deg2rad(positionList[i][0])
+    const radLongitudeB = deg2rad(positionList[i][1])
+
+    const radLatDiff = radLatitudeA - radLatitudeB
+    const radLonDiff = radLongitudeA - radLongitudeB
+
+    const radLatAve = (radLatitudeA + radLatitudeB) / 2.0;
+
+    const a = 6378137.0 // 赤道半径
+    const e2 = 0.00669438002301188 // 第一離心率^2
+    const a1e2 = 6335439.32708317 // 赤道上の子午線曲率半径
+
+    let sinLat = Math.sin(radLatAve)
+
+    sinLat = Math.sin(radLatAve)
+    let W2 = 1.0 - e2 * (sinLat*sinLat)
+    let M = a1e2 / (Math.sqrt(W2)*W2); // 子午線曲率半径M
+    let N = a / Math.sqrt(W2); // 卯酉線曲率半径
+
+    let t1 = M * radLatDiff
+    let t2 = N * Math.cos(radLatAve) * radLonDiff
+    let dist = Math.sqrt((t1*t1) + (t2*t2))
+
+    distance += dist
+  }
+  return distance
+}
+
+function deg2rad(deg){
+  return deg * ( Math.PI / 180 )
+}
+
 export default {
   data() {
     return {
       gpsTimerObj: null,
       isGpsIntervalSet: false,
       map: null,
+      startDate: new Date(),
+      startTime: 0
     }
   },
   methods:{
@@ -38,13 +83,19 @@ export default {
         this.gpsIntervalFunc() 
         this.gpsTimerObj = setInterval(this.gpsIntervalFunc, 5000) //5000 is ms
         this.isGpsIntervalSet = true
+        const tempDate = new Date()
+        this.startTime = tempDate.getTime()
       }
     },
     successGetGPS(position){
       const nowGPS = [position.coords.latitude, position.coords.longitude]
       this.$store.commit('addMyGPSLocation', nowGPS)
-      console.log('gps success')
       this.map.setView(nowGPS)
+
+      const tempDate = new Date()
+      this.$store.commit('addMyRunTimeList', tempDate.getTime() - this.startTime)
+
+      console.log('success')
     },
     gpsIntervalFunc(){
       console.log('get location')
@@ -70,22 +121,41 @@ export default {
       }
       coordinates += ']' 
 
-      let geoJson = ` { "type": "LineString",
-                      "crs": { "type": "name"
-                      },`;      
+      let geoJson = '{"type": "LineString","crs": { "type": "name"},'
       geoJson += '"coordinates":' + coordinates + '}'
 
-      console.log(geoJson)
-      // TODO 送信処理 
-      // TODO total timeとかを追加する
+      // TODO landmark_visitsの処理を追加する
       // ランドマークを回ったタイミングを出すには、それぞれのtimeが必要になる
+      const distance = calDistance(this.$store.state.myGPSLocations)
+      let postJson = {
+        "properties":{
+          "time_list": this.$store.state.myRunTimeList,
+          "total_distance": distance,
+          "total_time": this.$store.state.myRunTimeList[this.$store.state.myRunTimeList.length - 1]
+        },
+        "landmark_visits": [
+          {
+            "id": 1,
+            "time": 10000
+          },
+          {
+            "id": 2,
+            "time": 20000
+          }
+        ],
+        "geo_json":geoJson
+      }
+      console.log(postJson)
+      this.$postApi('/session/workout', postJson)
+    },
+    test(){
+      console.log(calDistance([[36.10377477777778, 140.08785502777778], [35.65502847222223, 139.74475044444443]]))
     }
   },
   mounted: function () {
     const mapDefaultZoomLevel = 17
     //MAP読み込み
     this.map = L.map('map').setView(SOJO_GPS_POSITION, mapDefaultZoomLevel);
-
     //map boxを現在使用しているが、より良いものがあったら変える
     L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
       attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
