02d3c07fa7fb90b95d6700122b139b20e070b303
KIBE <30930566+kibehiro@users.noreply.github.com>
2020-03-06 16:26:35 +0900

    比較部分の開発バージョンを作成

--- server/src/api/services/image.py
+++ server/src/api/services/image.py
@@ -1,4 +1,13 @@
+from usecases.image import compare_image
+
+
 class ImageService:
     @staticmethod
-    async def ImageCompare(image):
-        return "Image"
+    async def ImageCompare(image_request):
+        similarity = compare_image(
+            image_request.img, image_request.landmark_id)
+        threshold = 0.5
+        # similarityは、一致度が高いほど0に近くなるため
+        if similarity < threshold:
+            return "OK"
+        return "NG"
---
+++ server/src/api/usecases/image.py
@@ -0,0 +1,37 @@
+import cv2
+import numpy as np
+import base64
+from db.landmarks import get_image_url
+
+
+def decode_base64_image(image):
+    img_binary = base64.b64decode(image)
+    npimg = np.fromstring(img_binary, dtype=np.uint8)
+    img = cv2.imdecode(npimg, 1)
+    return img
+
+
+def compare_image(user_image, landmark_id):
+    # database_image = get_image_url(landmark_id)
+    database_image = decode_base64_image(user_image)
+
+    # FIXME: テスト段階だとuser_imageを先に変えるとdatabase_imageで死ぬ
+    user_image = decode_base64_image(user_image)
+
+    # A-KAZEとかいう検出器を使う
+    akaze = cv2.AKAZE_create()
+
+    # kpが特徴量, desが特徴量ベクトル
+    kp1, des1 = akaze.detectAndCompute(user_image, None)
+    kp2, des2 = akaze.detectAndCompute(database_image, None)
+
+    bf = cv2.BFMatcher()
+
+    # 特徴量ベクトル同士をBrute-Force＆KNNでマッチング
+    matches = bf.match(des1, des2)
+
+    dist = [m.distance for m in matches]
+    # 類似度を計算する
+    ret = sum(dist) / len(dist)
+
+    return ret
