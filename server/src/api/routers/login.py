fbd209da93ff61e048eb9a0104994f836f267065
Hiragi-GKUTH <hiragi.gkuth.dev@gmail.com>
2020-03-06 15:25:40 +0900

    fix verification skip on development

--- server/src/api/routers/login.py
+++ server/src/api/routers/login.py
@@ -13,6 +13,8 @@ async def get_login():
 @router.post("/", response_model=LoginResponse)
 async def post_login(loginRequest: LoginRequest):
     token = loginRequest.token
+    name = loginRequest.name
+    img_url = loginRequest.img_url
     message = await LoginService.register(token, name, img_url)
     response: LoginRequest = {
         "result": message
--- server/src/api/usecases/auth.py
+++ server/src/api/usecases/auth.py
@@ -1,7 +1,14 @@
+import os
+
 from firebase_admin import auth, exceptions
 from fastapi import HTTPException, status
+from random import randint
 
 def verify_token(token: str):
+  # development
+  if os.environ["SERVER_ENV"] == "development":
+    return "development_" + str(randint(0,1000000))
+
   try:
     dec_token = auth.verify_id_token(token)
     return dec_token["uid"]
