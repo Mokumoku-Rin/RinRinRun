fdb7a0ab084b7dc02a4f89d4ef4e921c766b5c01
Hiragi-GKUTH <hiragi.gkuth.dev@gmail.com>
2020-03-06 22:52:58 +0900

    add dummy user

--- db/migration/20-insert_dummy_user.sql
+++ db/migration/20-insert_dummy_user.sql
@@ -1,3 +1,4 @@
-INSERT INTO users (id) VALUES ("dummy_1");
-INSERT INTO users (id) VALUES ("dummy_2");
-INSERT INTO users (id) VALUES ("dummy_3");
+INSERT INTO `users` (`id`, `name`, `img_url`, `score`, `total_distance`, `total_time`) VALUES ('1', 'hiragi', 'https://1.bp.blogspot.com/-GMlc1rbn8IE/XaKa78Xe-VI/AAAAAAABVkc/ZnufoBxzH7syoQTw9XHOMd2EUxXCBLQ8wCNcBGAsYHQ/s1600/mensetsu_business_ai.png', '225', '234234', '13314414');
+INSERT INTO `users` (`id`, `name`, `img_url`, `score`, `total_distance`, `total_time`) VALUES ('2', 'kibe', 'https://1.bp.blogspot.com/-39XciIYPHcw/VpS_W8_Pm5I/AAAAAAAA20Y/PCYWg6kPu4k/s800/business_black_kigyou.png', '24355', '43534', '52353');
+INSERT INTO `users` (`id`, `name`, `img_url`, `score`, `total_distance`, `total_time`) VALUES ('3', 'yuu', 'https://2.bp.blogspot.com/-udq6-yh5Uwo/UbVvNJ8hc3I/AAAAAAAAUro/7g3NJ6uVfJQ/s800/computer_hacker.png', '44', '234', '23523432');
+INSERT INTO `users` (`id`, `name`, `img_url`, `score`, `total_distance`, `total_time`) VALUES ('4', 'tomohiro okada', 'https://1.bp.blogspot.com/-BmoKjF0YQq8/WWXW_-vVXUI/AAAAAAABFec/UZuemlIdlxMmsrQAgrUWSpsHXUP5JoxOACLcBGAs/s800/internet_dos_attack.png', '3', '4432', '4322');
