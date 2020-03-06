f8c433771225f9d7a64bb4206e2aa9d578aa27b7
KIBE <30930566+kibehiro@users.noreply.github.com>
2020-03-06 21:00:56 +0900

    set_intervalがDestroyされるようになっているはず (#67)
    * set_intervalがDestroyされるようになっているはず
    もし想定の動きと違ったりしてたらすまん
    あとAuto Formatかけちゃった……
    
    * デバッグ用のコメントを消し忘れてたので削除

--- client/src/src/components/CameraView.vue
+++ client/src/src/components/CameraView.vue
@@ -1,67 +1,78 @@
 <template>
-    <section id="camera-view">
-        <div><video ref="video" id="video" width="400" height="600" playsinline></video></div>
-        <canvas ref="canvas" id="canvas" width="400" height="600" disabled></canvas>
-    </section>
+  <section id="camera-view">
+    <div>
+      <video
+        ref="video"
+        id="video"
+        width="400"
+        height="600"
+        playsinline
+      ></video>
+    </div>
+    <canvas ref="canvas" id="canvas" width="400" height="600" disabled></canvas>
+  </section>
 </template>
 
 <script>
 export default {
-    data() {
-        return {
-            video: {},
-            canvas: {},
-        }
-    },
-    async mounted() {
-        // setup video
-        this.video = this.$refs.video;
-        const userAgent = navigator.userAgent.toLowerCase();
-        const stream = await getVideoStream(userAgent);
-        this.video.srcObject = stream;
-        this.video.play();
+  data() {
+    return {
+      video: {},
+      canvas: {},
+      interval: null
+    };
+  },
+  mounted: async function() {
+    // setup video
+    this.video = this.$refs.video;
+    const userAgent = navigator.userAgent.toLowerCase();
+    const stream = await getVideoStream(userAgent);
+    this.video.srcObject = stream;
+    this.video.play();
 
-        // setup capture interval
-        const interval = 1000; //1000ms is 1sec
-        setInterval(this.capture, interval)
-    },
-    async destroyed() {
-        clearInterval();
-    },
-    methods: {
-        capture() {
-            // this.captures.pop()
-            this.canvas = this.$refs.canvas
-            const imgData = this.canvas.toDataURL('image/png')
-            // TODO: 画像データから類似度を取る
-            console.log(imgData)
-        },
-    },
-}
+    // setup capture interval
+    const interval = 1000; //1000ms is 1sec
+    this.interval = setInterval(() => {
+      this.capture();
+    }, interval);
+  },
+  destroyed: async function() {
+    clearInterval(this.interval);
+  },
+  methods: {
+    capture: function() {
+      // this.captures.pop()
+      this.canvas = this.$refs.canvas;
+      const imgData = this.canvas.toDataURL("image/png");
+      // TODO: 画像データから類似度を取る
+      console.log(imgData);
+    }
+  }
+};
 
 async function getVideoStream(userAgent) {
-    const ua = userAgent.toLowerCase()
-    let medias = {}
+  const ua = userAgent.toLowerCase();
+  let medias = {};
 
-    // For iPhone support
-    if (ua.includes('iphone')) {
-        medias = {
-            audio: false,
-            video: {
-                facingMode: {
-                    exact: "environment"
-                }
-            }
+  // For iPhone support
+  if (ua.includes("iphone")) {
+    medias = {
+      audio: false,
+      video: {
+        facingMode: {
+          exact: "environment"
         }
-    } else {
-        medias = {
-            audio: false,
-            video: true,
-        }
-    }
+      }
+    };
+  } else {
+    medias = {
+      audio: false,
+      video: true
+    };
+  }
 
-    const videoStream = await navigator.mediaDevices.getUserMedia(medias);
+  const videoStream = await navigator.mediaDevices.getUserMedia(medias);
 
-    return videoStream;
+  return videoStream;
 }
 </script>
