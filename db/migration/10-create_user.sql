a62d43ab7674e1527e84fd5b6c7f1b77750a32d3
KIBE <30930566+kibehiro@users.noreply.github.com>
2020-03-07 15:54:39 +0900

    WIP insert dummy data (#69)
    * users table insert dummy data
    
    * add dummy user
    
    * add dummy workout, cource, landmark
    
    * create new table cource_landmarks for N-N relation tables
    
    * add dummy landmark, landmark_visit, cource_landmark
    
    * update actual landmarks dummy
    
    * new workout
    
    * cource -> course
    
    * お気持ちインデント修正
    
    Co-authored-by: Hiragi-GKUTH <hiragi.gkuth.dev@gmail.com>

--- db/migration/10-create_user.sql
+++ db/migration/10-create_user.sql
@@ -3,10 +3,10 @@
 CREATE TABLE IF NOT EXISTS `mokumoku`.`users` (
 	`id` VARCHAR(64) NOT NULL,
 	`name` VARCHAR(128) NOT NULL DEFAULT 'email user',
-	`img_url` VARCHAR(128) NOT NULL DEFAULT 'https://via.placeholder.com/128x128?text=User+Image',
+	`img_url` TEXT NOT NULL DEFAULT 'https://via.placeholder.com/128x128?text=User+Image',
 	`score` INT NOT NULL DEFAULT '0',
-	`total_distance` FLOAT NOT NULL DEFAULT '0' COMMENT 'unit km',
-	`total_time` TIME NOT NULL DEFAULT '0',
+	`total_distance` INT NOT NULL DEFAULT '0' COMMENT 'unit m',
+	`total_time` INT NOT NULL DEFAULT '0',
 	`created_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
 	`updated_at` TIMESTAMP NOT NULL ON UPDATE CURRENT_TIMESTAMP,
 	PRIMARY KEY (`id`)
--- server/src/api/db/workout.py
+++ server/src/api/db/workout.py
@@ -2,19 +2,19 @@ import pymysql.cursors
 from middlewares.database import get_db
 
 
-def insert_workout_history(uid, cource_id, geometry_track, time):
+def insert_workout_history(uid, course_id, geometry_track, time):
     conn = get_db()
     with conn.cursor() as cursor:
-        sql = "INSERT INTO workout_histories (user_id, cource_id, geometry_track, time) VALUE (%s, %s, %s, %s);"
-        cursor.execute(sql, (uid, cource_id, geometry_track, time))
+        sql = "INSERT INTO workout_histories (user_id, course_id, geometry_track, time) VALUE (%s, %s, %s, %s);"
+        cursor.execute(sql, (uid, course_id, geometry_track, time))
     conn.commit()
 
 
-def get_workout_history_id(uid, cource_id, geometry_track, time):
+def get_workout_history_id(uid, course_id, geometry_track, time):
     conn = get_db()
     with conn.cursor() as cursor:
-        sql = "SELECT id FROM workout_histories WHERE user_id = %s and cource_id = %s and geometry_track = %s and time = %s"
-        cursor.execute(sql, (uid, cource_id, geometry_track, time))
+        sql = "SELECT id FROM workout_histories WHERE user_id = %s and course_id = %s and geometry_track = %s and time = %s"
+        cursor.execute(sql, (uid, course_id, geometry_track, time))
         result = cursor.fetchone()
     return result
 
