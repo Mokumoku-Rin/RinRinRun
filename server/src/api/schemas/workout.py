7c9f28d880786aeeffa2d6194f56f4e6881f3586
KIBE <30930566+kibehiro@users.noreply.github.com>
2020-03-07 15:21:58 +0900

    upper_camelcaseに

--- server/src/api/schemas/workout.py
+++ server/src/api/schemas/workout.py
@@ -2,21 +2,21 @@ from pydantic import BaseModel
 from typing import List
 
 
-class property(BaseModel):
+class Property(BaseModel):
     time_list: List[int]
     total_distance: int
     total_time: int
     course_id: int
 
 
-class visit_landmark(BaseModel):
+class VisitLandmark(BaseModel):
     id: int
     time: int
 
 
 class WorkoutRequest(BaseModel):
-    properties: property
-    landmark_visits: List[visit_landmark]
+    properties: Property
+    landmark_visits: List[VisitLandmark]
     geo_json: str
 
 
