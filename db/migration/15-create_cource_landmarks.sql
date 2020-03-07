6dff4fd2ca4b336e6513751d34fcf1f0528dad77
Hiragi-GKUTH <hiragi.gkuth.dev@gmail.com>
2020-03-07 15:50:01 +0900

    お気持ちインデント修正

--- db/migration/15-create_cource_landmarks.sql
+++ db/migration/15-create_cource_landmarks.sql
@@ -3,6 +3,6 @@ CREATE TABLE IF NOT EXISTS `mokumoku`.`course_landmarks` (
     `course_id` INT unsigned NOT NULL,
     `landmark_id` INT unsigned NOT NULL,
     `created_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-	`updated_at` TIMESTAMP NOT NULL ON UPDATE CURRENT_TIMESTAMP,
-	PRIMARY KEY (`id`)
+    `updated_at` TIMESTAMP NOT NULL ON UPDATE CURRENT_TIMESTAMP,
+    PRIMARY KEY (`id`)
 )
