06074ad5d8024eb534f49af7545df716757fcc34
Hiragi-GKUTH <hiragi.gkuth.dev@gmail.com>
2020-03-07 15:15:48 +0900

    update actual landmarks dummy

--- db/migration/23-insert_dummy_landmark.sql
+++ db/migration/23-insert_dummy_landmark.sql
@@ -1,19 +1,19 @@
 INSERT INTO `landmarks`
 (`name`,`description`,`img_url`,`pos`)
 VALUES
-('K','is reseach tower!!','https://','1, 2');
+('From Bus Stop','Anyone see it at least once','https://livedoor.blogimg.jp/boomsports/imgs/4/8/488abd7a.jpg','34.879124, 135.575877');
 
 INSERT INTO `landmarks`
 (`name`,`description`,`img_url`,`pos`)
 VALUES
-('A','is professor tower!!','https://','4, 3');
+('When You become junior','Yes I chosen this one','https://f.easyuploader.app/eu-prd/upload/20200307150759_50663434595154657065.jpeg','34.878595, 135.575614');
 
 INSERT INTO `landmarks`
 (`name`,`description`,`img_url`,`pos`)
 VALUES
-('D','is master tower!!','https://','5, 1');
+('近道','知ってる人は知っている','https://f.easyuploader.app/eu-prd/upload/20200307150740_554457366a65464f4d76.jpeg','34.877803, 135.576536');
 
 INSERT INTO `landmarks`
 (`name`,`description`,`img_url`,`pos`)
 VALUES
-('L','is Lunch tower!!','https://','3, 3');
+('山','忘れてはいけない．ココは山である．','https://f.easyuploader.app/eu-prd/upload/20200307150806_4a73516e716458335449.jpeg','34.879803, 135.576925');
