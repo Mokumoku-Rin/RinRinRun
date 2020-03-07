aaa151a011a1cbce4e7dccfe07f2bbfa93463d55
Hiragi-GKUTH <hiragi.gkuth.dev@gmail.com>
2020-03-07 14:48:19 +0900

    add dummy landmark, landmark_visit, cource_landmark

--- db/migration/23-insert_dummy_landmark.sql
+++ db/migration/23-insert_dummy_landmark.sql
@@ -1,19 +1,19 @@
 INSERT INTO `landmarks`
-(`cource_id`,`name`,`description`,`img_url`,`pos`)
+(`name`,`description`,`img_url`,`pos`)
 VALUES
-('1','K','is reseach tower!!','https://','1, 2');
+('K','is reseach tower!!','https://','1, 2');
 
 INSERT INTO `landmarks`
-(`cource_id`,`name`,`description`,`img_url`,`pos`)
+(`name`,`description`,`img_url`,`pos`)
 VALUES
-('1','A','is professor tower!!','https://','4, 3');
+('A','is professor tower!!','https://','4, 3');
 
 INSERT INTO `landmarks`
-(`cource_id`,`name`,`description`,`img_url`,`pos`)
+(`name`,`description`,`img_url`,`pos`)
 VALUES
-('2','D','is master tower!!','https://','5, 1');
+('D','is master tower!!','https://','5, 1');
 
 INSERT INTO `landmarks`
-(`cource_id`,`name`,`description`,`img_url`,`pos`)
+(`name`,`description`,`img_url`,`pos`)
 VALUES
-('1','L','is Lunch tower!!','https://','3, 3');
+('L','is Lunch tower!!','https://','3, 3');
---
+++ db/migration/25-insert_dummy_cource_landmarks.sql
@@ -0,0 +1,4 @@
+INSERT INTO `cource_landmarks` (`cource_id`, `landmark_id`) VALUES ('1', '1')
+INSERT INTO `cource_landmarks` (`cource_id`, `landmark_id`) VALUES ('1', '2')
+INSERT INTO `cource_landmarks` (`cource_id`, `landmark_id`) VALUES ('2', '3')
+INSERT INTO `cource_landmarks` (`cource_id`, `landmark_id`) VALUES ('2', '4')
