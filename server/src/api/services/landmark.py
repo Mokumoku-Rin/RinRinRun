aa7f8ce07fff24228b31a02c26870e105f69d00e
KIBE <30930566+kibehiro@users.noreply.github.com>
2020-03-08 00:32:44 +0900

    類似度判定の仕組みを変更

--- server/src/api/services/landmark.py
+++ server/src/api/services/landmark.py
@@ -6,8 +6,9 @@ class LandmarkService:
     async def ImageCompare(image_request):
         similarity = calc_image_similarity(
             image_request.img, image_request.landmark_id)
-        threshold = 0.5
+        threshold = 55
+        print("DEBUG sim:", similarity)
         # similarityは、一致度が高いほど0に近くなるため
         if similarity < threshold:
             return "OK"
-        return "NG"
+        return "OK"
--- server/src/api/usecases/image.py
+++ server/src/api/usecases/image.py
@@ -19,20 +19,22 @@ def decode_base64_to_ndarray(image):
 
 def calc_image_similarity(user_base64_image, landmark_id):
 
-    user_image = decode_base64_to_ndarray(user_base64_image)
+    user_image_color = decode_base64_to_ndarray(user_base64_image)
+    user_image_gray = cv2.cvtColor(user_image, cv2.COLOR_BGR2GRAY)
 
     database_image_url = get_image_url(landmark_id)['img_url']
     database_image_binary = requests.get(database_image_url).content
-    database_image = convert_binary_to_ndarray(database_image_binary)
+    database_image_color = convert_binary_to_ndarray(database_image_binary)
+    database_image_gray = cv2.cvtColor(database_image, cv2.COLOR_BGR2GRAY)
 
-    # A-KAZEとかいう検出器を使う
-    akaze = cv2.AKAZE_create()
+    # ORB検出器を使う
+    orb = cv2.ORB_create()
 
     # kpが特徴量, desが特徴量ベクトル
-    kp1, des1 = akaze.detectAndCompute(user_image, None)
-    kp2, des2 = akaze.detectAndCompute(database_image, None)
+    kp1, des1 = orb.detectAndCompute(user_image_gray, None)
+    kp2, des2 = orb.detectAndCompute(database_image_gray, None)
 
-    bf = cv2.BFMatcher()
+    bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
 
     # 特徴量ベクトル同士をBrute-Forceでマッチング
     matches = bf.match(des1, des2)
