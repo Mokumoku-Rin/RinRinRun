b3b9c137066f3dd768edc829bd4a55b4c37956c2
Hiragi-GKUTH <hiragi.gkuth.dev@gmail.com>
2020-03-08 13:55:23 +0900

    add course description column

--- db/migration/12-create_cources.sql
+++ db/migration/12-create_cources.sql
@@ -1,6 +1,7 @@
 CREATE TABLE IF NOT EXISTS `mokumoku`.`courses` (
 	`id` INT unsigned NOT NULL AUTO_INCREMENT,
 	`name` VARCHAR(64) NOT NULL,
+	`description` TEXT NOT NULL DEFAULT '',
 	`mean_distance` INT unsigned NOT NULL,
 	`mean_time` INT unsigned NOT NULL COMMENT 'unit ms',
 	`played_count` INT unsigned NOT NULL DEFAULT '0',
