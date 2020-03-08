797ed85b61fed64715467fd06370c46ec94d9169
KIBE <30930566+kibehiro@users.noreply.github.com>
2020-03-08 14:48:05 +0900

    workoutがPostされたときにcoursesがアップデートされるように (#98)
    * coursesテーブルの平均がworkoutがポストされたときに更新されるように
    
    * update played count
    
    * 関数をまとめた

--- server/src/api/db/workout.py
+++ server/src/api/db/workout.py
@@ -24,3 +24,12 @@ def insert_landmark_visit(work_history_id, landmark_id, time):
         sql = "INSERT INTO landmark_visits (workout_history_id, landmark_id, time) VALUE (%s, %s, %s)"
         cursor.execute(sql, (work_history_id, landmark_id, time))
     conn.commit()
+
+
+def get_mean_total_record(course_id):
+    conn = get_db()
+    with conn.cursor() as cursor:
+        sql = "SELECT avg(total_time), avg(total_distance) FROM workout_histories WHERE course_id = %s"
+        cursor.execute(sql, (course_id))
+        result = cursor.fetchone()
+    return result
