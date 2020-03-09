264cb7f04e3c5157592c8e874db26837481ccd60
Hiragi-GKUTH <hiragi.gkuth.dev@gmail.com>
2020-03-09 11:13:43 +0900

    Cookie Originを消した

--- server/src/api/middlewares/cors.py
+++ server/src/api/middlewares/cors.py
@@ -12,7 +12,7 @@ def setup(app: FastAPI):
   app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
-    allow_credentials=True,
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
   )
