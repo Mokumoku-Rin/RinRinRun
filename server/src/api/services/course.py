68cf9aaafe3e69d005a0bc3867bf10e62e785d9c
KIBE <30930566+kibehiro@users.noreply.github.com>
2020-03-09 10:44:53 +0900

    最短時間と最短距離を返却するようにした (#111)
    * 最小距離と最小時間を取得するSQLを作成
    
    * 最短距離、最短時間を返すためにレスポンスのSchema定義を修正
    
    * 最短距離と最短時間を結合した辞書を返却するように
    
    * typoの修正

--- server/src/api/services/course.py
+++ server/src/api/services/course.py
@@ -1,4 +1,4 @@
-from usecases.course import get_course_list_by, get_cource
+from usecases.course import get_course_list_by, get_course
 from usecases.landmark import get_landmarks_for_course_request
 
 class CourseService:
@@ -13,6 +13,6 @@ class CourseService:
   
   @staticmethod
   def gether_course_info(course_id: int):
-    course_result = get_cource(course_id)
+    course_result = get_course(course_id)
     landmark_result = get_landmarks_for_course_request(course_id)
     return course_result, landmark_result
