b41c09ccd37863f155f0adae42e04876e2c8592a
KIBE <30930566+kibehiro@users.noreply.github.com>
2020-03-10 20:13:08 +0900

    100番代から始まるように

--- db/migration/32-courses.sql
+++ db/migration/32-courses.sql
@@ -1,7 +1,7 @@
 INSERT INTO `courses` 
-(`name`, `mean_distance`, `mean_time`, `played_count`) 
-VALUES ('ALL_RUN', '4300', '566524', '10');
+(`id`,`name`, `mean_distance`, `mean_time`, `played_count`) 
+VALUES ('100', 'ALL_RUN', '4300', '566524', '10');
 
 INSERT INTO `courses` 
-(`name`, `mean_distance`, `mean_time`, `played_count`) 
-VALUES ('Short Cource', '7600', '765254', '28');
+(`id`, `name`, `mean_distance`, `mean_time`, `played_count`) 
+VALUES ('101', 'Short Cource', '7600', '765254', '28');
--- db/migration/35-course_landmarks.sql
+++ db/migration/35-course_landmarks.sql
@@ -1,26 +1,26 @@
 INSERT INTO `course_landmarks`
-(`course_id`, `landmark_id`) VALUES ('1', '1');
+(`id`, `course_id`, `landmark_id`) VALUES ('100', '100', '100');
 
 INSERT INTO `course_landmarks` 
-(`course_id`, `landmark_id`) VALUES ('1', '2');
+(`id`, `course_id`, `landmark_id`) VALUES ('101', '100', '101');
 
 INSERT INTO `course_landmarks` 
-(`course_id`, `landmark_id`) VALUES ('1', '3');
+(`id`, `course_id`, `landmark_id`) VALUES ('102', '100', '102');
 
 INSERT INTO `course_landmarks` 
-(`course_id`, `landmark_id`) VALUES ('1', '4');
+(`id`, `course_id`, `landmark_id`) VALUES ('103', '100', '103');
 
 INSERT INTO `course_landmarks` 
-(`course_id`, `landmark_id`) VALUES ('1', '5');
+(`id`, `course_id`, `landmark_id`) VALUES ('104', '100', '104');
 
 INSERT INTO `course_landmarks` 
-(`course_id`, `landmark_id`) VALUES ('1', '6');
+(`id`, `course_id`, `landmark_id`) VALUES ('105', '100', '105');
 
 INSERT INTO `course_landmarks` 
-(`course_id`, `landmark_id`) VALUES ('2', '6');
+(`id`, `course_id`, `landmark_id`) VALUES ('106', '101', '105');
 
 INSERT INTO `course_landmarks` 
-(`course_id`, `landmark_id`) VALUES ('2', '4');
+(`id`, `course_id`, `landmark_id`) VALUES ('107', '101', '103');
 
 INSERT INTO `course_landmarks` 
-(`course_id`, `landmark_id`) VALUES ('2', '2');
+(`id`, `course_id`, `landmark_id`) VALUES ('108', '101', '101');
