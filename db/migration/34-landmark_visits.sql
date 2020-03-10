e4632209cc6b28b58c7842825f636bc723ca9461
KIBE <30930566+kibehiro@users.noreply.github.com>
2020-03-10 20:19:43 +0900

    landmark_visitsのダミーも合わせて作成

---
+++ db/migration/34-landmark_visits.sql
@@ -0,0 +1,17 @@
+INSERT INTO `landmark_visits` (`id`, `workout_history_id`, `landmark_id`, `time`)
+VALUES ('100', '100', '100', '1000');
+
+INSERT INTO `landmark_visits` (`id`, `workout_history_id`, `landmark_id`, `time`)
+VALUES ('101', '100', '101', '3000');
+
+INSERT INTO `landmark_visits` (`id`, `workout_history_id`, `landmark_id`, `time`)
+VALUES ('102', '100', '103', '1000');
+
+INSERT INTO `landmark_visits` (`id`, `workout_history_id`, `landmark_id`, `time`)
+VALUES ('103', '100', '105', '3000');
+
+INSERT INTO `landmark_visits` (`id`, `workout_history_id`, `landmark_id`, `time`)
+VALUES ('104', '100', '104', '1000');
+
+INSERT INTO `landmark_visits` (`id`, `workout_history_id`, `landmark_id`, `time`)
+VALUES ('105', '100', '102', '3000');
