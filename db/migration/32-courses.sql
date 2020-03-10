e4782719c213c891faf74b22b6a10ed8ccb58ee0
KIBE <30930566+kibehiro@users.noreply.github.com>
2020-03-10 14:47:58 +0900

    ダミーデータを追加

---
+++ db/migration/32-courses.sql
@@ -0,0 +1,7 @@
+INSERT INTO `courses` 
+(`name`, `mean_distance`, `mean_time`, `played_count`) 
+VALUES ('ALL_RUN', '4300', '566524', '10');
+
+INSERT INTO `courses` 
+(`name`, `mean_distance`, `mean_time`, `played_count`) 
+VALUES ('Short Cource', '7600', '765254', '28');
---
+++ db/migration/35-course_landmarks.sql
@@ -0,0 +1,26 @@
+INSERT INTO `course_landmarks`
+(`course_id`, `landmark_id`) VALUES ('1', '1');
+
+INSERT INTO `course_landmarks` 
+(`course_id`, `landmark_id`) VALUES ('1', '2');
+
+INSERT INTO `course_landmarks` 
+(`course_id`, `landmark_id`) VALUES ('1', '3');
+
+INSERT INTO `course_landmarks` 
+(`course_id`, `landmark_id`) VALUES ('1', '4');
+
+INSERT INTO `course_landmarks` 
+(`course_id`, `landmark_id`) VALUES ('1', '5');
+
+INSERT INTO `course_landmarks` 
+(`course_id`, `landmark_id`) VALUES ('1', '6');
+
+INSERT INTO `course_landmarks` 
+(`course_id`, `landmark_id`) VALUES ('2', '6');
+
+INSERT INTO `course_landmarks` 
+(`course_id`, `landmark_id`) VALUES ('2', '4');
+
+INSERT INTO `course_landmarks` 
+(`course_id`, `landmark_id`) VALUES ('2', '2');
