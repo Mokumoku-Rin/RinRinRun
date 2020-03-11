0e62d62901528e1807e6a245bedbd9d4053ac867
KIBE <30930566+kibehiro@users.noreply.github.com>
2020-03-11 12:14:21 +0900

    ダミーデータを追加 (#123)
    * ダミーデータを追加
    
    * 100番代から始まるように
    
    * landmark_visitsのダミーも合わせて作成
    
    * delete space
    
    * fix time

---
+++ db/migration/35-course_landmarks.sql
@@ -0,0 +1,26 @@
+INSERT INTO `course_landmarks`
+(`id`, `course_id`, `landmark_id`) VALUES ('100', '100', '100');
+
+INSERT INTO `course_landmarks` 
+(`id`, `course_id`, `landmark_id`) VALUES ('101', '100', '101');
+
+INSERT INTO `course_landmarks` 
+(`id`, `course_id`, `landmark_id`) VALUES ('102', '100', '102');
+
+INSERT INTO `course_landmarks` 
+(`id`, `course_id`, `landmark_id`) VALUES ('103', '100', '103');
+
+INSERT INTO `course_landmarks` 
+(`id`, `course_id`, `landmark_id`) VALUES ('104', '100', '104');
+
+INSERT INTO `course_landmarks` 
+(`id`, `course_id`, `landmark_id`) VALUES ('105', '100', '105');
+
+INSERT INTO `course_landmarks` 
+(`id`, `course_id`, `landmark_id`) VALUES ('106', '101', '105');
+
+INSERT INTO `course_landmarks` 
+(`id`, `course_id`, `landmark_id`) VALUES ('107', '101', '103');
+
+INSERT INTO `course_landmarks` 
+(`id`, `course_id`, `landmark_id`) VALUES ('108', '101', '101');
