5c702e24584c52746dcbdc0029a256926c6d97b5
KIBE <30930566+kibehiro@users.noreply.github.com>
2020-03-11 16:00:40 +0900

    sqlの実行をdatabase.pyで実行するように

--- server/src/api/db/workout.py
+++ server/src/api/db/workout.py
@@ -1,62 +1,42 @@
-import pymysql.cursors
-from middlewares.database import get_db
+from middlewares.database import (
+    execute_fetchone, execute_fetchall, execute_commit)
 
 
 def insert_workout_history(uid, course_id, total_time,
                            total_distance, time_list, geo_json):
-    conn = get_db()
-    with conn.cursor() as cursor:
-        sql = "INSERT INTO workout_histories (user_id, course_id, total_time, total_distance, time_list, geo_linestring) VALUE (%s, %s, %s, %s, %s, ST_GeomFromGeoJSON(%s));"
-        cursor.execute(sql, (uid, course_id, total_time,
-                             total_distance, time_list, geo_json))
-        sql = "SELECT LAST_INSERT_ID();"
-        cursor.execute(sql)
+    sql = "INSERT INTO workout_histories (user_id, course_id, total_time, total_distance, time_list, geo_linestring) VALUE (%s, %s, %s, %s, %s, ST_GeomFromGeoJSON(%s));"
+    execute_commit(sql, (uid, course_id, total_time,
+                         total_distance, time_list, geo_json))
 
-        insert_id = cursor.fetchone()['LAST_INSERT_ID()']
-    conn.commit()
-
+    sql = "SELECT LAST_INSERT_ID();"
+    insert_id = execute_fetchone(sql)['LAST_INSERT_ID()']
     return insert_id
 
 
 def insert_landmark_visit(work_history_id, landmark_id, time):
-    conn = get_db()
-    with conn.cursor() as cursor:
-        sql = "INSERT INTO landmark_visits (workout_history_id, landmark_id, time) VALUE (%s, %s, %s)"
-        cursor.execute(sql, (work_history_id, landmark_id, time))
-    conn.commit()
+    sql = "INSERT INTO landmark_visits (workout_history_id, landmark_id, time) VALUE (%s, %s, %s)"
+    execute_commit(sql, (work_history_id, landmark_id, time))
 
 
 def get_shortest(course_id):
-    conn = get_db()
-    with conn.cursor() as cursor:
-        sql = "SELECT MIN(total_time), MIN(total_distance) FROM workout_histories WHERE course_id = %s"
-        cursor.execute(sql, (course_id))
-        result = cursor.fetchone()
+    sql = "SELECT MIN(total_time), MIN(total_distance) FROM workout_histories WHERE course_id = %s"
+    result = execute_fetchone(sql, (course_id))
     return result
 
 
 def get_mean_total_record(course_id):
-    conn = get_db()
-    with conn.cursor() as cursor:
-        sql = "SELECT avg(total_time), avg(total_distance) FROM workout_histories WHERE course_id = %s"
-        cursor.execute(sql, (course_id))
-        result = cursor.fetchone()
+    sql = "SELECT avg(total_time), avg(total_distance) FROM workout_histories WHERE course_id = %s"
+    result = execute_fetchone(sql, (course_id))
     return result
 
 
 def get_totaldist_timelist_poslist(uid, course_id):
-    conn = get_db()
-    with conn.cursor() as cursor:
-        sql = "SELECT id, time_list, ST_AsGeoJSON(geo_linestring), total_distance, total_time FROM workout_histories WHERE user_id = %s AND course_id = %s ORDER BY total_time"
-        cursor.execute(sql, (uid, course_id))
-        result = cursor.fetchone()
+    sql = "SELECT id, time_list, ST_AsGeoJSON(geo_linestring), total_distance, total_time FROM workout_histories WHERE user_id = %s AND course_id = %s ORDER BY total_time"
+    result = execute_fetchone(sql, (uid, course_id))
     return result
 
 
 def get_based_on_date(uid, date):
-    conn = get_db()
-    with conn.cursor() as cursor:
-        sql = "SELECT total_time, total_distance, ST_AsGeoJSON(geo_linestring) FROM workout_histories WHERE user_id = %s AND created_at LIKE %s"
-        cursor.execute(sql, (uid, date))
-        result = cursor.fetchall()
+    sql = "SELECT total_time, total_distance, ST_AsGeoJSON(geo_linestring) FROM workout_histories WHERE user_id = %s AND created_at LIKE %s"
+    result = execute_fetchall(sql, (uid, date))
     return result
