6f3df68df4534829d945c6d87c4d2d889984bc73
KIBE <30930566+kibehiro@users.noreply.github.com>
2020-03-11 23:12:51 +0900

    スキーマの定義

--- server/src/api/schemas/landmark.py
+++ server/src/api/schemas/landmark.py
@@ -11,10 +11,21 @@ class LandmarkVisitRequest(BaseModel):
     course_id: int
     img: str
 
+
 class LandmarkInfo(BaseModel):
     id: int
     name: str
     pos: str
     img_url: str
     description: str
-    
+
+
+class LandmarkPostResponse(BaseModel):
+    result: str
+
+
+class LandmarkPostRequest(BaseModel):
+    name: str
+    description: str
+    img_url: str
+    pos: str
