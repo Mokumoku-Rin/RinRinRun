d040ef78dbff8b372589c91049a532f50971ca3b
myabu <dptypo@gmail.com>
2020-03-12 21:25:32 +0900

    fix-api-env (#152)

--- client/src/src/plugins/rinrin-api.js
+++ client/src/src/plugins/rinrin-api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 import firebase from 'firebase/app'
 
-const API_ENDPOINT = process.env["API_ENDPOINT"]
+const API_ENDPOINT = process.env["VUE_APP_API_ENDPOINT"]
 
 function postApiErrorDefaultFunc(error){
   console.log('---rinrin-api-plugin-$postApi--postToApi-error-----')
