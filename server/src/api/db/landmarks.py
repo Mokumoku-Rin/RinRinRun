25860e878615cf5ab7958de9001eca79946e0984
KIBE <30930566+kibehiro@users.noreply.github.com>
2020-03-12 13:50:47 +0900

    とりあえずランドマークがPostできるように (#143)
    * スキーマの定義
    
    * ルーティングの定義
    
    * サービスの定義
    
    * ユースケースの定義
    
    * dbの定義

--- server/src/api/db/landmarks.py
+++ server/src/api/db/landmarks.py
@@ -12,3 +12,8 @@ def get_landmarks_by_course_id(course_id: int):
     sql = 'SELECT landmarks.id, landmarks.name, landmarks.description, landmarks.img_url, landmarks.pos FROM landmarks JOIN course_landmarks ON landmarks.id = course_landmarks.landmark_id JOIN courses ON course_landmarks.course_id = courses.id WHERE courses.id = %s'
     result = execute_fetchall(sql, (str(course_id)))
     return result
+
+
+def insert_landmark(name, description, img_url, pos):
+    sql = 'INSERT INTO landmarks (name, description, img_url, pos) VALUES (%s, %s, %s, %s)'
+    execute_commit(sql, (name, description, img_url, pos))
