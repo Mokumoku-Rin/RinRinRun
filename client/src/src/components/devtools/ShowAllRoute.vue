ef0833619c0a8bb100131ff168bece0c27822612
yuu <dptypo@gmail.com>
2020-03-12 19:53:15 +0900

    fix route

--- client/src/src/components/devtools/ShowAllRoute.vue
+++ client/src/src/components/devtools/ShowAllRoute.vue
@@ -6,9 +6,8 @@
     <router-link to="/choice-course">コース選択</router-link><br>
     <router-link to="/take-picture">写真をとるページ</router-link><br>
     <router-link to="/run-map-test">ゴースト表示テスト</router-link><br>
-    <router-link to="/running-info?course_id=1">ランニング中のページ</router-link>
-    <router-link to="/running-goal">ゴールしたときのページ</router-link>
-    <router-link to="/running-goal">ゴールしたときのページ</router-link>
+    <router-link to="/running-info?course_id=1">ランニング中のページ</router-link><br>
+    <router-link to="/running-goal">ゴールしたときのページ</router-link><br>
   </div>
 </template>
 
